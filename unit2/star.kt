f2874f8b11fa1990d8e7cb6395089255f726e15e
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-06 09:01:32 -0600

    Update star.kt

--- unit2/star.kt
+++ unit2/star.kt
@@ -1,3 +1,19 @@
-fun main () {
-
-}
+    internal object StarPattern {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var x = 1
+            var increment_by = 2
+            while (x > 0) {
+// print spaces
+                val spaces = 4 - x / 2
+                for (i in 1..spaces) print(" ")
+                // print asterisks
+                for (i in 1..x) print("*")
+                // end line
+                println()
+                // change width
+                if (x == 7) increment_by = -2 // reverse
+                x += increment_by
+            }
+        }
+    }

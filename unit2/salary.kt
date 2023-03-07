ea4c2c00f4e5598df1726cf486432a1562224b13
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-07 14:45:26 -0600

    Update salary.kt

--- unit2/salary.kt
+++ unit2/salary.kt
@@ -1,11 +1,13 @@
-fun main (){
-    var name = "Farnaz Ghodsianzadeh"
-    val age = 28
-    val annualPay = 9000000: Double
+fun main () {
+    var name: String
+    val age: Int
+    val annualPay: Double
+
+    name = "Farnaz Ghodsianzadeh"
+    age = 28
+    annualPay = 9000000.00
 
     println("My name is " + name)
     println(", and my age is " + age)
     println("and I hope to earn " + annualPay)
-
-
 }

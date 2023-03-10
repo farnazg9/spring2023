435a0f10df85686c7e633ff9ce4a99dc1a9e40de
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-09 19:54:22 -0600

    Update variables.kt

--- unit2/variables.kt
+++ unit2/variables.kt
@@ -1,17 +1,17 @@
 fun main() {
     val myAge = 28
-    var averageAge = 28
-    averageAge = 28 % 30
+    var averageAge = (myAge + 30) / 2.0
     val testNumber = 9
-    val evenOdd = "testNumber % 2"
-    var variousNumbers = "testNumber"
-    var answer = 0
-    var answer = " + 1"
-    answer = "answer + 10"
-    answer = "answer * 10"
-    answer = ">> 3"
+    val evenOdd = testNumber % 2
+   // when using different test numbers, 1 means modulo
+   // is present and is odd and 0 means it is not present and is an even number
 
-    println("myAge")
+    var answer = 0
+    answer = answer + 1
+    answer = answer + 10
+    answer = answer * 10
+    answer = answer shr 3
+    //answer is 12
 
 
 }

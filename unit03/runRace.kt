3715baed2ac4c975f0881e899f9a0a7cc840d776
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-31 10:06:31 -0500

    Update runRace.kt

--- unit03/runRace.kt
+++ unit03/runRace.kt
@@ -1,56 +1,35 @@
-import javax.swing.JOptionPane
+fun main() {
 
-object RunningTheRace {
-    @JvmStatic
-    fun main(args: Array<String>) {
+    val name1: String
+    val name2: String
+    val name3: String
+    val time1: String
+    val time2: String
+    val time3: String
 
-        // Declare variables to store user input
-        val name1: String
-        val name2: String
-        val name3: String
-        val time1: String
-        val time2: String
-        val time3: String
+    val t1: Double
+    val t2: Double
+    val t3: Double
 
+    val name1 = first
+    val name2 = second
+    val name3 = third
+    val time1 = last
+    val time2 = middle
+    val time3 = current
 
-        val t1: Double
-        val t2: Double
-        val t3: Double
+    t1 = time1.toDouble()
+    t2 = time2.toDouble()
+    t3 = time3.toDouble()
 
-
-        name1 = JOptionPane.showInputDialog("Enter the first runners name")
-        name2 = JOptionPane.showInputDialog("Enter the second runners name")
-        name3 = JOptionPane.showInputDialog("Enter the third runners name")
-
-
-        time1 = JOptionPane.showInputDialog("Enter the time runner 1 finished the race")
-        time2 = JOptionPane.showInputDialog("Enter the time runner 2 finished the race")
-        time3 = JOptionPane.showInputDialog("Enter the time runner 3 finished the race")
-
-
-        t1 = time1.toDouble()
-        t2 = time2.toDouble()
-        t3 = time3.toDouble()
-
-
-        if (t1 > t2 && t1 > t3) {
+    if (t1 > t2 && t1 > t3) {
             if (t2 > t3) {
-                JOptionPane.showMessageDialog(null, "1st Place: $name1\n2nd Place: $name2\n3rd Place: $name3")
+               println("1st Place: $name1\n2nd Place: $name2\n3rd Place: $name3")
             } else {
-                JOptionPane.showMessageDialog(null, "1st Place: $name1\n2nd Place: $name3\n3rd Place: $name2")
-            }
-        } else if (t2 > t1 && t2 > t3) {
-            if (t1 > t3) {
-                JOptionPane.showMessageDialog(null, "1st Place: $name2\n2nd Place: $name1\n3rd Place: $name3")
-            } else {
-                JOptionPane.showMessageDialog(null, "1st Place: $name2\n2nd Place: $name3\n3rd Place: $name1")
+               println("1st Place: $name1\n2nd Place: $name3\n3rd Place: $name2")
             }
         } else if (t3 > t2 && t3 > t1) {
-            if (t2 > t1) {
-                JOptionPane.showMessageDialog(null, "1st Place: $name3\n2nd Place: $name2\n3rd Place: $name1")
-            } else {
-                JOptionPane.showMessageDialog(null, "1st Place: $name3\n2nd Place: $name1\n3rd Place: $name2")
-            }
-        }
-    }
-}
+           if (t2 > t1) {
+              println("1st Place: $name3\n2nd Place: $name2\n3rd Place: $name1")
+            } 
+    }}

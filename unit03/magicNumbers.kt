77600d5dc04558cf6bec92f8911062175783a625
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-31 09:08:50 -0500

    Update magicNumbers.kt

--- unit03/magicNumbers.kt
+++ unit03/magicNumbers.kt
@@ -1,10 +1,15 @@
-fun main () {
-    month=int(input('Enter month (numeric):'))
-    day=int(input('Enter day:'))
-    year=int(input('Enter two digit year:'))
-    day_month=day*month
-    if day_month==year:
-    print('This date is magic!')
-    else:
-    print('This date is not magic.')
-}
+fun main() {
+
+    val Month: Int = 1
+
+    val Day: Int = 1
+
+    val Year: Int = 1
+
+    if (Year % Month == 0)
+
+        println("This date is MAGIC!")
+
+    else if (Year % Month != 0)
+
+        println("This date is not magic!")

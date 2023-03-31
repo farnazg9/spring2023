ac42eaf7887cfce7f9a06c3702310dc833a632fc
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-31 12:04:02 -0500

    Update bankCharges.kt

--- unit03/bankCharges.kt
+++ unit03/bankCharges.kt
@@ -1,11 +1,7 @@
-import java.util.*
+fun main() {
 
-object BankCharges {
-    @JvmStatic
-    fun main(args: Array<String>) {
-
-        //Bank Charges
-        val BanksFee = 10.00 // PER MONTH
+        
+       val BanksFee = 10.00 
         val LessThan20 = 0.10
         val Between20_39 = 0.08
         val Between40_59 = 0.06
@@ -14,13 +10,13 @@ object BankCharges {
         var Checks = 0
 
 
-        //ASKING USER
-        val userInput = Scanner(System.`in`)
+       
+       val userInput = Scanner(System.`in`)
         println("Please enter the number of checks written this month:")
         Checks = userInput.nextInt()
         if (Checks < 20) {
             TotalFee = Checks * LessThan20 + BanksFee
-            print("This month's service fees are $ $TotalFee")
+           print("This month's service fees are $ $TotalFee")
         } else if (Checks == 20 || Checks < 40) {
             TotalFee = Checks * Between20_39 + BanksFee
             print("This month's service fees are $ $TotalFee")
@@ -29,7 +25,5 @@ object BankCharges {
             print("This month's service fees are $ $TotalFee")
         } else if (Checks > 60) {
             TotalFee = Checks * MoreThan60 + BanksFee
-            print("This month's service fees are $ $TotalFee")
-        }
-    }
-}
+           print("This month's service fees are $ $TotalFee")
+        }}

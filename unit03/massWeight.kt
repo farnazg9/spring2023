b3f836a43f06b438e0cd7e9e33e6aceffc68b4d5
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-03-31 09:31:55 -0500

    Update massWeight.kt

--- unit03/massWeight.kt
+++ unit03/massWeight.kt
@@ -1,8 +1,15 @@
-Mass = float(input("Enter an object’s mass :"))                         #Asking user to enter an object's mass.
-Weight = Mass * 9.8                                                      # Converting mass in kilograms to weight in newtons.
-Message = ""
-if Weight > 1000:                                                       #Checking if weight is above 1000 or not.
-Message = " The object is too heavy at " + format(Weight, ',.2f') +" newtons."     # If it is true update the message.
-elif Weight < 10:                                                         #Checking if weight is below 10 or not.
-Message = "The object is too light at"  + format(Weight, ',.2f') + " newtons." # if it is true update the message.
-print(Message)                                                                # Display the result
+fun main() {
+val mass = 10
+
+val weight = mass * 9.8
+
+
+if (weight < 100) 
+   println("The object is too light")
+    
+if (weight >= 100) 
+   println("The object is too heavy")
+
+
+    
+         }

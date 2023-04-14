b19f5b48a108f0a04bd01c450c57329612b6b48c
farnazg9 <113859492+farnazg9@users.noreply.github.com>
2023-04-14 10:02:38 -0500

    Add files via upload

---
+++ unit04/sumNumbers.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var x = 0
+    var sum = 0
+
+    while (x <= 50) {
+        sum + = x
+                println(sum)
+        println("x = " + x + " sum = " + sum)
+        x = 51 sum = 12
+    }
+}
